842ae1ed4185a983bc5283163eda9a2271b7acfe
Geison Souza <geisonpixpicpay@gmail.com>
2026-07-13 21:38:37 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Angels Estética Animal - Firebase Config
+window.firebaseEnabled = true;
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyBod60eSEo-zfVsa4XD_ZzWVzKpJlsZVcM",
+  authDomain: "angels-agenda.firebaseapp.com",
+  projectId: "angels-agenda",
+  storageBucket: "angels-agenda.firebasestorage.app",
+  messagingSenderId: "796463233277",
+  appId: "1:796463233277:web:471e0796be00b489812694",
+  measurementId: "G-Y2M68MDTGR"
+};
